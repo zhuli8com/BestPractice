3a202fba3e6e9dad2db35f06bc51b555733f10e3
zhuli <zhuli1228@163.com>
2015-10-26 16:36:52 +0800

    添加启动图片和appicon

--- BestPractice/BestPractice/ViewController.m
+++ BestPractice/BestPractice/ViewController.m
@@ -9,7 +9,6 @@
 #import "ViewController.h"
 
 @interface ViewController ()
-@property (weak, nonatomic) IBOutlet UIButton *button;
 
 @end
 
@@ -18,21 +17,21 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
-#ifdef _APP_DEV_
-    [self.button setTitle:@"我是开发环境" forState:UIControlStateNormal];
-#endif
-    
-#ifdef _APP_TEST_
-    [self.button setTitle:@"我是TEST环境" forState:UIControlStateNormal];
-#endif
-    
-#ifdef _APP_RC_
-    [self.button setTitle:@"我是RC环境" forState:UIControlStateNormal];
-#endif
-    
-#ifdef _APP_APPSTORE_
-    [self.button setTitle:@"我是APPSTORE环境" forState:UIControlStateNormal];
-#endif
+//#ifdef _APP_DEV_
+//    [self.button setTitle:@"我是开发环境" forState:UIControlStateNormal];
+//#endif
+//    
+//#ifdef _APP_TEST_
+//    [self.button setTitle:@"我是TEST环境" forState:UIControlStateNormal];
+//#endif
+//    
+//#ifdef _APP_RC_
+//    [self.button setTitle:@"我是RC环境" forState:UIControlStateNormal];
+//#endif
+//    
+//#ifdef _APP_APPSTORE_
+//    [self.button setTitle:@"我是APPSTORE环境" forState:UIControlStateNormal];
+//#endif
 }
 
 - (void)didReceiveMemoryWarning {
