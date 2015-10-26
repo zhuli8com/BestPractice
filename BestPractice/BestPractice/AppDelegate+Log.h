96e8196ad8732ec4babbbc52604c9df8bcf64ae0
zhuli <zhuli1228@163.com>
2015-10-26 18:19:33 +0800

    添加日志的使用注释和appdelegate日志分类

---
+++ BestPractice/BestPractice/AppDelegate+Log.h
@@ -0,0 +1,13 @@
+//
+//  AppDelegate+Log.h
+//  BestPractice
+//
+//  Created by lizhu on 15/10/26.
+//  Copyright © 2015年 zhuli. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate (Log)
+
+@end
