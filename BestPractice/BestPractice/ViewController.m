8e56ed85da09b1f411f3b798118ec33f106ad444
zhuli <zhuli1228@163.com>
2015-10-26 10:46:27 +0800

    搭建git环境

---
+++ BestPractice/BestPractice/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  BestPractice
+//
+//  Created by lizhu on 15/10/26.
+//  Copyright © 2015年 zhuli. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ BestPractice/BestPractice/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  BestPractice
+//
+//  Created by lizhu on 15/10/26.
+//  Copyright © 2015年 zhuli. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
