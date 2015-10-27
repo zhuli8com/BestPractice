01a280ab360551b468f33cd2e3e12d9508338b97
zhuli <zhuli1228@163.com>
2015-10-27 16:36:10 +0800

    添加配置文件

---
+++ BestPractice/BestPractice/AppConfig.h
@@ -0,0 +1,32 @@
+//
+//  AppConfig.h
+//  BestPractice
+//
+//  Created by lizhu on 15/10/27.
+//  Copyright © 2015年 zhuli. All rights reserved.
+//
+
+//一共有四套环境：开发环境、测试环境、预上线环境、发布环境
+#ifndef AppConfig_h
+#define AppConfig_h
+
+#ifdef _APP_DEV_
+
+#define _URL_SCHEME_    @"jfdev"
+#endif
+
+#ifdef _APP_TEST_
+
+#define _URL_SCHEME_    @"jftest"
+#endif
+
+#ifdef _APP_RC_
+
+#define _URL_SCHEME_    @"jfrc"
+#endif
+
+#ifdef _APP_APPSTORE_
+
+#define _URL_SCHEME_    @"jointforce"
+#endif
+#endif /* AppConfig_h */
